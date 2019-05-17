342cbd3334d580dff84460322f4c53301291f806
github username <github email address>
2019-05-17 10:00:46 -0500

    node.js

--- node.js
+++ node.js
@@ -1,5 +1,6 @@
 /*
  how to see hidden files, run these in terminal:
  defaults write com.apple.finder AppleShowAllFiles YES  
- rm -rf .git delete the repo
+ rm -rf .git delete the repo this is to delete from our local computer
+ git rm --cached react-sing/    run this in terminal, this is to delete github version so we don't have submodule
 */
